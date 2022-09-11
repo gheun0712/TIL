2da7bdecbff58ce1c8aab7de4442bcf006e7bbd2
gheun0712 <gheun071212@gmail.com>
2022-09-11 22:12:10 +0900

    update hello-react

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,11 +1,13 @@
 function App() {
   const name = '리액트';
-  return (
-    <>
-      <h1>{name} 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </>
-  );
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  };
+  return <div style={style}> {name} </div>;
 }
 
 export default App;
