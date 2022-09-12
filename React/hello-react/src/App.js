9a5b1309297478db6c80bfb102ed606a00e61b61
gheun0712 <gheun071212@gmail.com>
2022-09-12 23:03:50 +0900

    update hello-react

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  const name = '리액트';
-  return <div className="react"> {name} </div>;
+  const name = "리액트";
+  return (
+    <>
+      {/*주석은 이렇게 작성*/}
+      <div className="react">{name}</div>
+      //이런 주석이나 /*이런 주석은 페이지에 그대로 나옴 */
+      <input />
+    </>
+  );
 }
 
 export default App;
