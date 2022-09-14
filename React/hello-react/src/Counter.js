e6e553dd7c1b3d29b77fcd6dc766e5527bd2ef4b
gheun0712 <gheun071212@gmail.com>
2022-09-14 22:12:36 +0900

    update hello-react

--- React/hello-react/src/Counter.js
+++ React/hello-react/src/Counter.js
@@ -23,10 +23,10 @@ class Counter extends Component {
           onClick={() => {
             this.setState(
               {
-                number: number + 1
+                number: number + 1,
               },
               () => {
-                console.log('방금 setState가 호출되었습니다.');
+                console.log("방금 setState가 호출되었습니다.");
                 console.log(this.state);
               }
             );
