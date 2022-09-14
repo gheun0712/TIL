dd7963a2c3dade80ef37aff4fecf5874a6546efe
gheun0712 <gheun071212@gmail.com>
2022-09-14 21:23:17 +0900

    update hello-react

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,7 +1,7 @@
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 const App = () => {
-  return <MyComponent name="김" favoriteNumber={1}>리액트</MyComponent>;
+  return <Counter />;
 };
 
 export default App;
