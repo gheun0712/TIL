4c156ce066a5aeb597652da08e4bab65a936c640
gheun0712 <gheun071212@gmail.com>
2022-09-14 21:36:14 +0900

    update hello-react

--- React/hello-react/src/Counter.js
+++ React/hello-react/src/Counter.js
@@ -1,21 +1,37 @@
 import { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+  // constructor(props) {
+  //   super(props);
+  //   // state의 초깃값 설정하기
+  //   this.state = {
+  //     number: 0,
+  //     fixedNumber: 0
+  //   };
+  // }
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            // 위 코드와 아래 코드는 완전히 똑같은 기능
+            // 아래 코드는 함수에서 바로 객체를 반환함
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+            // this.setState({ number: number + 1 });
           }}
         >
           + 1
