a1de36fe5444470bbffc4ef4ed62dc37ade8c9e3
gheun0712 <gheun071212@gmail.com>
2022-09-22 22:15:53 +0900

    add IterationSample

---
+++ React/hello-react/src/IterationSample.js
@@ -0,0 +1,12 @@
+const IterationsSample = () => {
+  return (
+    <ul>
+      <li>눈사람</li>
+      <li>얼음</li>
+      <li>눈</li>
+      <li>바람</li>
+    </ul>
+  );
+};
+
+export default IterationsSample;
