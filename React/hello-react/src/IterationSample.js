175d3f56833f0d8939d4b989def07d18997b77bf
gheun0712 <gheun071212@gmail.com>
2022-09-23 23:59:23 +0900

    update IterationSample

--- React/hello-react/src/IterationSample.js
+++ React/hello-react/src/IterationSample.js
@@ -1,12 +1,13 @@
 const IterationsSample = () => {
-  return (
-    <ul>
-      <li>눈사람</li>
-      <li>얼음</li>
-      <li>눈</li>
-      <li>바람</li>
-    </ul>
-  );
-};
+  const names = ['눈사람', '얼음', '눈', '바람'];
+//   return (
+//     <ul>
+//       <li>눈사람</li>
+//       <li>얼음</li>
+//       <li>눈</li>
+//       <li>바람</li>
+//     </ul>
+//   );
+// };
 
 export default IterationsSample;
