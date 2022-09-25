e3a7799403689e8d13e3de11913b95343e9673b2
gheun0712 <gheun071212@gmail.com>
2022-09-25 23:04:26 +0900

    update IterationSample

--- React/hello-react/src/IterationSample.js
+++ React/hello-react/src/IterationSample.js
@@ -1,8 +1,14 @@
-const IterationsSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map(name => <li>{name}</li>);
+const IterationSample = () => {
+  const names = ["눈사람", "얼음", "눈", "바람"];
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
   return <ul>{nameList}</ul>;
 };
+
+// const IterationsSample = () => {
+//   const names = ['눈사람', '얼음', '눈', '바람'];
+//   const nameList = names.map(name => <li>{name}</li>);
+//   return <ul>{nameList}</ul>;
+// };
 //   return (
 //     <ul>
 //       <li>눈사람</li>
@@ -13,4 +19,4 @@ const IterationsSample = () => {
 //   );
 // };
 
-export default IterationsSample;
+export default IterationSample;
